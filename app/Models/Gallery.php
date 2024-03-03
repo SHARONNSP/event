ba267c8d0aba0001aff6011e0307bb868341b10e
sharonnsp96 <sharonnsp@appengage.asia>
2024-03-03 22:21:31 +0800

    update code

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -2,10 +2,43 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Gallery extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'image',
+        'caption'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function galleries(): BelongsTo
+    {
+        return $this->belongsTo(Gallery::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->HasMany(Comment::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->HasMany(Like::class);
+    }
+
+    public function attendings(): HasMany
+    {
+        return $this->HasMany(Attending::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +18,23 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Country::create(['name' => 'Malaysia']);
+        Country::create(['name' => 'United Kingdom']);
+        City::create(['country_id' => '1 ','name' => 'Puchong']);
+        City::create(['country_id' => '1 ','name' => 'Shah Alam']);
+        City::create(['country_id' => '1 ','name' => 'Klang']);
+        City::create(['country_id' => '1 ','name' => 'Subang']);
+        City::create(['country_id' => '1 ','name' => 'Puchong']);
+        City::create(['country_id' => '2 ','name' => 'London']);
+        City::create(['country_id' => '2 ','name' => 'Liverpool']);
+
+        Tag::create(['name' => 'Laravel', 'slug' => 'laravel']);
+        Tag::create(['name' => 'Vue JS', 'slug' => 'Vue JS']);
+        Tag::create(['name' => 'Livewire', 'slug' => 'Livewire']);
     }
 }
